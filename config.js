95209fd852df5a9cf4d2e732a60387c4bffaefd0
ScottAgirs <scott@ijs.to>
2021-12-20 11:20:56 +0200

    feat: Init config/URLS
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ config.js
@@ -0,0 +1,4 @@
+export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
+export const GRAPHQL_URL = `${process.env.NEXT_PUBLIC_API_URL}/graphql`;
+
+export const CLIENT_URL = `${process.env.NEXT_PUBLIC_CLIENT_URL}`;
