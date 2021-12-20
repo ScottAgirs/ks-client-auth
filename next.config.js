fc11beb39f12afbdf1f2fece1f1c29d239f8b3f6
ScottAgirs <scott@ijs.to>
2021-12-20 13:51:43 +0200

    chore: add wildcard to /api redirect
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api/graphql',
-        destination: 'https://ks-api-auth-o6629.ondigitalocean.app/api/graphql',
-      },
+        source: '/api*',
+        destination: 'https://ks-api-auth-o6629.ondigitalocean.app/api*',
+      }
     ];
   },
 };
