96d74d964db1a85d36777f91e865ca17ed3895fe
ScottAgirs <scott@ijs.to>
2021-12-20 13:53:13 +0200

    refactor: patch matching wildcard
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/api*',
-        destination: 'https://ks-api-auth-o6629.ondigitalocean.app/api*',
+        source: '/api/:path*',
+        destination: 'https://ks-api-auth-o6629.ondigitalocean.app/api/:path*',
       }
     ];
   },
